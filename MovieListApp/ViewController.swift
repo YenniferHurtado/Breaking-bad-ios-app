d04f051af16ff776b6faeaeadf27ed0842767ccf
Yennifer Hurtado <yhurtado@everis.com>
2021-03-06 12:29:04 -0500

    initialize project

---
+++ MovieListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListApp
+//
+//  Created by Yennifer Jhoselin Hurtado Arce on 3/6/21.
+//  Copyright © 2021 Yennifer Jhoselin Hurtado Arce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
