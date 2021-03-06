3b62969c88e52fd5a933a2e14819d3867fe77f7e
Yennifer Hurtado <yhurtado@everis.com>
2021-03-06 15:27:07 -0500

    view main programmatically

---
+++ MovieListApp/Main/View/ViewControllerCollectionView.swift
@@ -0,0 +1,44 @@
+//
+//  ViewControllerCollectionView.swift
+//  MovieListApp
+//
+//  Created by Yennifer Jhoselin Hurtado Arce on 3/6/21.
+//  Copyright © 2021 Yennifer Jhoselin Hurtado Arce. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerCollectionView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    private var collectionView: UICollectionView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        collectionView.register(ViewControllerCollectionViewCell.self, forCellWithReuseIdentifier: ViewControllerCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ViewControllerCollectionViewCell.identifier, for: indexPath)
+        
+        return cell
+    }
+
+
+}
+
---
+++ MovieListApp/Main/View/ViewControllerCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerCollectionViewCell.swift
+//  MovieListApp
+//
+//  Created by Yennifer Jhoselin Hurtado Arce on 3/6/21.
+//  Copyright © 2021 Yennifer Jhoselin Hurtado Arce. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ViewControllerCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .systemRed
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(code:) has not implemented")
+    }
+    
+}
