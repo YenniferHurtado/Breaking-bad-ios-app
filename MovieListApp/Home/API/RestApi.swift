9210f8478c112fed5198f100c5cc326f4015f763
Yennifer Hurtado <yhurtado@everis.com>
2021-03-09 13:31:55 -0500

    modified callRestApi with Observable return

--- MovieListApp/Home/API/RestApi.swift
+++ MovieListApp/Home/API/RestApi.swift
@@ -11,30 +11,13 @@ import Alamofire
 import RxSwift
 
 protocol APIServiceProtocol {
-    func callRestApi(url: String)
+    func callResApiData(url: String) -> Observable<[EpisodeModel]>
 }
 
 public class RestApi: APIServiceProtocol {
     
     public static let sharedInstance = RestApi()
     var characterModel = [EpisodeModel]()
-    
-    func callRestApi(url: String) {
-        
-        AF.request(url).response { response in
-            if let data = response.data {
-                do {
-                    let response = try JSONDecoder().decode([EpisodeModel].self, from: data)
-                    self.characterModel.append(contentsOf: response)
-                    DispatchQueue.main.async {
-                        print(response)
-                    }
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            }
-        }
-    }
     
     func callResApiData(url: String) -> Observable<[EpisodeModel]> {
         
